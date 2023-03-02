ec7d36fa65e07d7d12b1e3450bd8aed47904c9c6
Cneon90 <kirillkalita2@gmail.com>
2023-03-02 22:13:32 +0500

    Vue setup ON

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestDrive></TestDrive>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TestDrive from "@/components/TestDrive";
+
+export default {
+  name: 'App',
+  components: {
+    TestDrive,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/TestDrive.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <b-button @click="clickMe">Click Me</b-button>
+    </section>
+</template>
+
+<script>
+export default {
+  name: "TestDrive",
+  methods: {
+            clickMe() {
+                this.$buefy.notification.open('Clicked!!')
+            }
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
